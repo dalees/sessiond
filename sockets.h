99f83fbae017d59a6b6140c94981b2bdb885db77
Dale Smith <dale@catalyst.net.nz>
2012-11-08 14:46:14 +1300

    Added sockets.h, required by client

---
+++ sockets.h
@@ -0,0 +1,17 @@
+/*
+ * Common header for a bunch of files to include
+ * @author Dale Smith
+*/
+
+#ifndef __SOCKETS_H
+#define __SOCKETS_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+
+#endif //__SOCKETS_H
+ 
